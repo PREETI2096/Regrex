a20b3f1c820e108c307704211a71155273a6ff31
PREETI2096 <preetirg96@gmail.com>
2023-02-08 09:45:29 +0530

    Added: first name regrex pattern

---
+++ src/com/bridgelabz/regrex/UserRegistration.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.regrex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	public void validFirstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first name : ");
+		String firstName = sc.nextLine();
+
+		boolean fName = Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName);
+
+		if (fName) {
+			System.out.println("firstname is correct ");
+		} else {
+			System.out.println("firstname is incorrect ");
+		}
+	}
+}
