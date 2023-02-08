de2e6fa60fdfb64cd340b978bcf47852d105b5af
PREETI2096 <preetirg96@gmail.com>
2023-02-08 12:09:51 +0530

    Added: Email Id regrex pattern

--- src/com/bridgelabz/regrex/UserRegistration.java
+++ src/com/bridgelabz/regrex/UserRegistration.java
@@ -16,6 +16,7 @@ public class UserRegistration {
 			System.out.println("firstname is incorrect ");
 		}
 	}
+
 	public void validLastName() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the last name : ");
@@ -28,5 +29,18 @@ public class UserRegistration {
 		} else {
 			System.out.println("lastname is incorrect ");
 		}
-}
+	}
+
+	public void validEmailId() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter email id");
+		String email = sc.nextLine();
+
+		boolean validEmail = Pattern.matches("^[a-z]*.[a-z]+@[a-z]+.[a-z]{2,3}(.[a-z]{2,})*$", email);
+		if (validEmail) {
+			System.out.println("Email is valid");
+		} else {
+			System.out.println("Email is invalid");
+		}
+	}
 }
